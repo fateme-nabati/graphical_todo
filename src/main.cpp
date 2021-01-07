fed3cde8ac1ecf1033a9ccffd799e270d5f71c7b
fateme-nabati <fatemenabati22@gmail.com>
2021-01-07 10:24:54 -0800

    change names to meaningfull names and add text :  *** + add a task *** in bottom of window

--- src/main.cpp
+++ src/main.cpp
@@ -11,29 +11,44 @@ int main()
   sf::Event event;
 
   // background
-  sf::Texture texture;
+  sf::Texture backgroundTexture;
+  sf::Sprite backgroundSpr;
+  backgroundTexture.loadFromFile("/home/fn/Downloads/Webp.net-resizeimage(1).jpg");
+  backgroundSpr.setTexture(backgroundTexture);
+  // picture part of date and time
+  /*sf::Texture texture;
   sf::Sprite spr;
   texture.loadFromFile("/home/fn/Downloads/Webp.net-resizeimage(1).jpg");
-  spr.setTexture(texture);
+  spr.setTexture(texture);*/
 
   //icon calendar
-  sf::Texture texture2;
-  sf::Sprite spr2;
-  texture2.loadFromFile("/home/fn/Downloads/Webp.net-resizeimage.png");
-  spr2.setTexture(texture2);
-  spr2.setPosition(sf::Vector2f(10, 0));
+  sf::Texture calendarTexture;
+  sf::Sprite calendarSpr;
+  calendarTexture.loadFromFile("/home/fn/Downloads/Webp.net-resizeimage.png");
+  calendarSpr.setTexture(calendarTexture);
+  calendarSpr.setPosition(sf::Vector2f(10, 0));
 
   //date and time
-  sf::Text txt;
-  sf::Font font;
-  font.loadFromFile("/home/fn/sfml/OpenSans-Semibold.ttf");
+  sf::Text timeTxt;
+  sf::Font timeFont;
+  timeFont.loadFromFile("/home/fn/sfml/OpenSans-Semibold.ttf");
   time_t timetoday;
   time (&timetoday);
   string s = static_cast<string> (asctime(localtime(&timetoday)));
-  txt.setFont(font);
-  txt.setFillColor(sf::Color::Red);
-  txt.setString(s);
-  txt.setPosition(sf::Vector2f((spr2.getGlobalBounds().width + 20), 0));
+  timeTxt.setFont(timeFont);
+  timeTxt.setFillColor(sf::Color::Red);
+  timeTxt.setString(s);
+  timeTxt.setPosition(sf::Vector2f((calendarSpr.getGlobalBounds().width + 20), 0));
+
+  // a part for add a task
+  sf::Text addTxt;
+  sf::Font addFont;
+  addFont.loadFromFile("/home/fn/sfml/OpenSans-Semibold.ttf");
+  addTxt.setFont(addFont);
+  addTxt.setFillColor(sf::Color::Red);
+  addTxt.setString("+ add a task");
+  //addTxt.setOrigin(sf::Vector2f(0, -1*addTxt.getGlobalBounds().height));
+  addTxt.setPosition(sf::Vector2f(0, window.getSize().y- 2*addTxt.getGlobalBounds().height));
 
   while(window.isOpen())
   {
@@ -45,9 +60,10 @@ int main()
         window.close();
       }
     }
-    window.draw(spr);// draw picture of background
-    window.draw(spr2);// draw calendar icon
-    window.draw(txt);// draw date and time
+    window.draw(backgroundSpr);// draw picture of background
+    window.draw(calendarSpr);// draw calendar icon
+    window.draw(timeTxt);// draw date and time
+    window.draw(addTxt);// draw + add a task
     window.display();// display window
   }
 
